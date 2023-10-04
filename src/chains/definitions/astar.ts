60b976ecdaa3ca9764496d2df3817dfefc8adac7
Yuri Gii <abovezenith@gmail.com>
2023-10-04 22:45:09 +0200

    add astar network

---
+++ src/chains/definitions/astar.ts
@@ -0,0 +1,39 @@
+import { defineChain } from '../../utils/chain.js'
+
+export const astar = /*#__PURE__*/ defineChain({
+    id: 592,
+    name: 'Astar',
+    network: 'astar',
+    nativeCurrency: {
+        decimals: 18,
+        name: 'ASTR',
+        symbol: 'ASTR',
+    },
+    rpcUrls: {
+        public: {
+            http: ['https://evm.astar.network'],
+            webSocket: ['wss://rpc.astar.network'],
+        },
+        default: {
+            http: ['https://evm.astar.network'],
+            webSocket: ['wss://rpc.astar.network'],
+        },
+    },
+    blockExplorers: {
+        default: {
+            name: 'Blockscout',
+            url: 'https://blockscout.com/astar',
+        },
+        etherscan: {
+            name: 'Blockscout',
+            url: 'https://blockscout.com/astar',
+        },
+    },
+    contracts: {
+        multicall3: {
+            address: '0xcA11bde05977b3631167028862bE2a173976CA11',
+            blockCreated: 761794,
+        },
+    },
+    testnet: false,
+})
